591f6ef3112e77fdc703bfbb5a1583278defba79
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2023-05-17 19:45:00 +0300

    Add files via upload

--- ShowPage.xaml.cs
+++ ShowPage.xaml.cs
@@ -12,9 +12,21 @@ namespace XF2_443_SQLDB1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ShowPage : ContentPage
     {
+        Users user;
         public ShowPage(Users user)
         {
+            
             InitializeComponent();
+            this.user = user;
+            Logout.Clicked += (s, e) => Navigation.PopAsync();
+            Username.Text = "Welcome: "+ user.Username;
+            ShowMe.Text = user.Id + "    " + user.Username + "    " + user.Password + "\n";
+        }
+
+        private void Update_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new UpdatePage(user));
+            
         }
     }
 }
